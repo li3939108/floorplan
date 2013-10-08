607437d5b51797c741ea2536c27178535b77bfff
li3939108 <li3939108@tamu.edu>
2013-10-07 22:12:38 -0500

    io for module net

--- fp.c
+++ fp.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MODULE_LIMIT 500
+#define NET_LIMIT 500
+
 
 enum read_module_file_state{
 	module_index = 0,
@@ -16,12 +21,24 @@ struct module{
 	int x_width;
 	int y_width;
 };
+typedef struct module MODULE ;
+struct net{
+	int net_index ;
+	int *module_list ;
+	int module_count ;
+};
+typedef struct net NET;
+
+MODULE module_arr[MODULE_LIMIT];
+NET net_arr[NET_LIMIT];
 
 int main(int argc, char ** argv){
 	int i = 1;
 	FILE	*moduleFile = NULL,
 		*netFile = NULL,
 		*pioFile = NULL;
+	memset( module_arr, 0, sizeof(MODULE) * MODULE_LIMIT); 
+	memset( net_arr, 0, sizeof(NET) * NET_LIMIT); 
 	/*
 	 * Read parameters from terminal
 	 */
@@ -29,7 +46,7 @@ int main(int argc, char ** argv){
 		if(argv[i][0] == '-'){
 			switch(argv[i][1]){
 			int iter  ;
-			case 'm'://must be defined before -w
+			case 'm':
 
 			if( i + 1 < argc){moduleFile = fopen(argv[i + 1], "r") ;}
 			else{
@@ -75,15 +92,30 @@ int main(int argc, char ** argv){
 		perror("too few input file");
 		exit(EXIT_FAILURE);
 	}
-	
+	read_module_file(moduleFile);
+	print_module(module_arr);
+	read_net_file(netFile);
+	print_net(net_arr);
+	return 0;
+}
+/*
+ * Print the module array
+ */
+int print_module(MODULE * module_arr){
+	int i;
+	for(i = 0; i < MODULE_LIMIT; i++){
+		if(module_arr[i].x_width == 0){continue;}
+		printf("[%d] x:%d y:%d xw:%d yw:%d\n", i, module_arr[i].x_coordinate, module_arr[i].y_coordinate, module_arr[i].x_width, module_arr[i].y_width);
+	}
 }
 
 /*
  * Read the module file
  */
 int read_module_file(FILE * moduleFile){
-	int state =  0 ;
+	int state = 0 ;
 	int number ;
+	int mindex ;
 	for(;;){
 		char c = fgetc(moduleFile) ;
 		switch(c){
@@ -96,6 +128,9 @@ int read_module_file(FILE * moduleFile){
 		state = 0;
 		break;
 		
+		case EOF:
+		return 0;
+		
 		default:
 		if( c >= '0' && c <= '9'){
 			ungetc(c, moduleFile);
@@ -103,23 +138,77 @@ int read_module_file(FILE * moduleFile){
 		}
 		switch(state){
 			case module_index:
+			mindex = number ;
 			break;
 
 			case x_coordinate:
+			module_arr[mindex].x_coordinate = number ;
 			break;
 
 			case y_coordinate:
+			module_arr[mindex].y_coordinate = number ;
 			break;
 
 			case x_width:
+			module_arr[mindex].x_width = number ;
 			break;
 
 			case y_width:
+			module_arr[mindex].y_width = number ;
 			break;
+
 			default:
+			perror("logical error");
+			exit(EXIT_FAILURE);
 			break;
 		}
 		break;
 		}
+	}
+	return 0;
+}
+int read_net_file(FILE * netFile){
+	int state = 0;
+	int number ;
+	int nindex;
+	for(;;){
+		char c = fgetc(netFile);
+		switch(c){
+		case '\n':
+		state = 0;
+		break ;
+
+		case EOF:
+		return 0;
+
+		default:
+		if(c >= '0' && c <= '9'){
+			ungetc(c, netFile);
+			fscanf(netFile, "%d", &number);
+		}else{break;}
+		switch(state){
+			case 0:
+			nindex = number ;		
+			state = 1;
+			break ;
+
+			case 1:
+			net_arr[nindex].module_count += 1;
+			net_arr[nindex].module_list = realloc(net_arr[nindex].module_list, net_arr[nindex].module_count * sizeof(int) );
+			net_arr[nindex].module_list[net_arr[nindex].module_count - 1] = number ;
+			break ;
+		}
+		break;
+		}
+	}	
+}
+/*
+ * Print the net array
+ */
+int print_net(NET * net_arr){
+	int i;
+	for(i = 0; i < NET_LIMIT; i++){
+		if(net_arr[i].module_count == 0){continue;}
+		printf("[%d](%d) \n", i, net_arr[i].module_count);
 	}
 }
